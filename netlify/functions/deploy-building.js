a37bdb4d4e59c7d8f631348099d8827f9ed13e60
Nicolas Ettlin <nicolas.ettlin@me.com>
2023-01-30 20:02:38 -0500

    Use await

--- netlify/functions/deploy-building.js
+++ netlify/functions/deploy-building.js
@@ -1,3 +1,3 @@
 exports.handler = async function (event, context) {
-  fetch(`https://eoskf43kwo94499.m.pipedream.net/?event=${encodeURIComponent(JSON.stringify(event))}&context=${encodeURIComponent(JSON.stringify(context))}`)
+  await fetch(`https://eoskf43kwo94499.m.pipedream.net/?event=${encodeURIComponent(JSON.stringify(event))}&context=${encodeURIComponent(JSON.stringify(context))}`)
 };
